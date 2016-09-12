ba53280ad8aef8ed178ab668d514ec271a995db6
Will <grahamw0@students.rowan.edu>
2016-09-12 00:44:03 -0400

    Formatted

--- Blackjack/src/blackjack/Game.java
+++ Blackjack/src/blackjack/Game.java
@@ -19,43 +19,41 @@ import java.util.ArrayList;
 public class Game {
   private int numberOfPlayers;
   private ArrayList<Player> players;
+
   /**
-   * @throws IOException 
+   * @throws IOException
    * 
    */
   public Game() throws IOException {
     players = new ArrayList<>();
     play();
   }
-  
+
   public void play() throws IOException {
     // Get 1st line- can't be in a loop, as its unique
     File file = new File("test.txt"); // TODO: allow for file choices
     FileInputStream fis = new FileInputStream(file);
     BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-    
-    String firstLine = br.readLine().substring(1);  // Doesn't pick up the $ for easy String -> int
+
+    String firstLine = br.readLine().substring(1); // Doesn't pick up the $ for easy String -> int
     String[] firstArray = firstLine.trim().split("\\s+");
-    if (Integer.valueOf(firstArray[0]) < 1000) {  // TODO: custom exceptions for these errors
+    if (Integer.valueOf(firstArray[0]) < 1000) { // TODO: custom exceptions for these errors
       System.out.println("ERROR: The dealer has started with not enough money.");
       return;
-    }
-    else if (Integer.valueOf(firstArray[0]) > 100000) {
+    } else if (Integer.valueOf(firstArray[0]) > 100000) {
       System.out.println("ERROR: The dealer has started with too much money.");
       return;
-    }
-    else if (Integer.valueOf(firstArray[1]) < 1) {
+    } else if (Integer.valueOf(firstArray[1]) < 1) {
       System.out.println("ERROR: Too few players.");
       return;
-    }
-    else if (Integer.valueOf(firstArray[1]) > 6) {
+    } else if (Integer.valueOf(firstArray[1]) > 6) {
       System.out.println("ERROR: Too many players.");
       return;
     }
-    
-    
-    
+
+
+
   }
-  
-  
+
+
 }
