4f4f53281edbc26bdd82dbe953a9bcdb83eb75d1
Will <grahamw0@students.rowan.edu>
2016-09-16 00:41:53 -0400

    Moved 1st line error checking to seperate method- reduces clutter

--- Blackjack/src/blackjack/Game.java
+++ Blackjack/src/blackjack/Game.java
@@ -23,7 +23,7 @@ public class Game {
   private int numberOfPlayers;
   private ArrayList<Player> players; // Includes dealer as last player
   private ArrayList<String> outputArray;
-  private File file = new File("src/dataFiles/test.txt");  // TODO: allow for file choices
+  private File file = new File("src/dataFiles/test.txt"); // TODO: allow for file choices
 
   /**
    * @throws IOException
@@ -43,37 +43,10 @@ public class Game {
     String firstLine = br.readLine().substring(1); // Doesn't pick up the $ for easy String -> int
     String[] firstArray = firstLine.trim().split("\\s+");
 
-    // TODO: custom exceptions for these errors
-    boolean firstLineError = false;
-    if (Integer.valueOf(firstArray[0]) < 1000) {
-      System.out.println("ERROR: The dealer has started with not enough money.");
-      outputArray.add("ERROR: The dealer has started with not enough money.");
-      firstLineError = true;
-    }
-    if (Integer.valueOf(firstArray[0]) > 100000) {
-      System.out.println("ERROR: The dealer has started with too much money.");
-      outputArray.add("ERROR: The dealer has started with too much money.");
-      firstLineError = true;
-    }
-    if (Integer.valueOf(firstArray[1]) < 1) {
-      System.out.println("ERROR: Too few players.");
-      outputArray.add("ERROR: Too few players.");
-      firstLineError = true;
-    }
-    if (Integer.valueOf(firstArray[1]) > 6) {
-      System.out.println("ERROR: Too many players.");
-      outputArray.add("ERROR: Too many players.");
-      firstLineError = true;
-    }
-    if (firstLineError) {
-      br.close();
-      writeOutput();
-    } else {
-      System.out.println("everything's cool");
-      outputArray.add("everything's cool");
-    }
+    
+    firstLineErrorCheck(firstArray);
 
-    numberOfPlayers = Integer.valueOf(firstArray[1]) + 1; // # of players includes dealer
+    numberOfPlayers = Integer.valueOf(firstArray[1]); // # of players includes dealer
 
     for (int i = 0; i < numberOfPlayers; i++) {
       players.add(new Player());
@@ -81,6 +54,7 @@ public class Game {
 
     // Ensures the dealer is always the last "player" in the list
     players.add(new Player(Integer.valueOf(firstArray[0])));
+    numberOfPlayers++;  // Accounts for dealer
 
 
     String gameLine = null;
@@ -101,17 +75,48 @@ public class Game {
 
   }
 
-  private void writeOutput() throws IOException {
+  private void exit() throws IOException {
     BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
     writer.append("\n");
     writer.append("\n");
     writer.append("OUTPUT:\n");
-    for(String line : outputArray) {
-      writer.append(line);
+    for (String line : outputArray) {
+      writer.append(line + "\n");
     }
-    
+
     writer.close();
     System.exit(0);
+  }
+
+  private void firstLineErrorCheck(String[] array) throws IOException {
+    // TODO: custom exceptions for these errors
+    boolean firstLineError = false;
+    if (Integer.valueOf(array[0]) < 1000) {
+      System.out.println("ERROR: The dealer has started with not enough money.");
+      outputArray.add("ERROR: The dealer has started with not enough money.");
+      firstLineError = true;
+    }
+    if (Integer.valueOf(array[0]) > 100000) {
+      System.out.println("ERROR: The dealer has started with too much money.");
+      outputArray.add("ERROR: The dealer has started with too much money.");
+      firstLineError = true;
+    }
+    if (Integer.valueOf(array[1]) < 1) {
+      System.out.println("ERROR: Too few players.");
+      outputArray.add("ERROR: Too few players.");
+      firstLineError = true;
+    }
+    if (Integer.valueOf(array[1]) > 6) {
+      System.out.println("ERROR: Too many players.");
+      outputArray.add("ERROR: Too many players.");
+      firstLineError = true;
+    }
+    if (firstLineError) {
+      exit();
+    } else {
+      System.out.println("everything's cool");
+      outputArray.add("everything's cool");
+    }
   }
 
 
