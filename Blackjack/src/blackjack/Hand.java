78d5b1c4f7792261d27c1cda3b4f27ee1fd5a18c
Will <grahamw0@students.rowan.edu>
2016-09-21 22:10:25 -0400

    Updated comments

--- Blackjack/src/blackjack/Hand.java
+++ Blackjack/src/blackjack/Hand.java
@@ -1,141 +1,135 @@
-/**
- * 
- */
 package blackjack;
 
 /**
- * This is class "Hand".  It will control the logic
- * of what is entailed in a hand of blackjack. 
- *   
+ * This is class "Hand". It will control the logic of what is entailed in a hand of blackjack.
+ * 
  * @author Ryan Godfrey, Will Graham
- * @version 1.0		9/18/2016
+ * @version 1.0 9/18/2016
  *
  */
 public class Hand {
-  private Card[] cards; // Array of Card called cards
-  private int total; // The total of all cards in a hand.
-  private boolean bust; // Boolean value of whether the hand is bust or not.  
-  
+  private Card[] cards;
+  private int total;
+  private boolean bust; // Quick way to get whether a hand is bust or not.
+
   /**
-   * The constructor for class Hand. It will set bust to false
-   * and initialize cards to null.  
+   * The constructor for class Hand. It will set bust to false and initialize cards to null.
    */
 
   public Hand() {
-    bust = false; // bust is initialized to false.
-    this.cards = null; // Cards set equal to null.  
-    //calculateTotal();
-    //this.bust = isBust(this.total);
+    bust = false;
+    this.cards = null;
     bust = false;
   }
-  
+
   /**
-   * Overloaded constructor for class Hand.  Set's bust to false
-   * and initializes cards to itself.  
+   * Overloaded constructor for class Hand. Set's bust to false and initializes cards to itself.
+   * 
    * @param cards
    */
   public Hand(Card[] cards) {
-    bust = false; // bust is initialized to false
-    this.cards = cards; // Might throw error
-    calculateTotal(); // Calls on the calculateTotal() method.  
+    bust = false;
+    this.cards = cards;
+    calculateTotal();
   }
 
   /**
-   * The calculateTotal method holds the logic on how
-   * much each card is worth from an integer standpoint.  
+   * The calculateTotal method holds the logic on how much each card is worth from an integer
+   * standpoint.
    */
   private void calculateTotal() {
-    boolean hasAnAce = false; // Set hasAnAce to false.  If the hand has an Ace this will be changed to true.  
-    int softTotal = 0; // Create an int called softTotal and set it to equal 0.
-    int hardTotal = 0; // Create an int called hardTotal and set it to equal 0.  
-    // Create a for each loop that iterates through the cards.   
+    boolean hasAnAce = false;
+    int softTotal = 0; // Assumes any aces will be counted as only 1.
+    int hardTotal = 0; // The first ace will be 11, all others must be 1.
     for (Card card : cards) {
-      if (card.isAce() && !hasAnAce) { // If the card is an Ace and hasAnAce is true
-        hardTotal += Card.HIGH_ACE; // The hardTotal 's Ace is worth 11.
-        softTotal += 1; // The softTotal 's Ace is worth 1.
-        hasAnAce = true; // Set hasAnAce to true.  
-      } else if (card.isAce() && hasAnAce) { // If the card is not an Ace and hasAcAce is false
-        hardTotal += 1; // The Ace is worth 1. 
-        softTotal += 1; // The Ace is worth 1.
+      if (card.isAce() && !hasAnAce) { // If the card is an ane and there isn't already one.
+        hardTotal += Card.HIGH_ACE; // The first ace for hardTotal is always 11.
+        softTotal += 1;
+        hasAnAce = true;
+      } else if (card.isAce() && hasAnAce) { // A second or more ace- both totals get a 1.
+        hardTotal += 1;
+        softTotal += 1;
       } else {
-        hardTotal += card.getValue(); // Get the value of the hardTotal.
-        softTotal += card.getValue(); // Get the value of the softTotal. 
+        hardTotal += card.getValue();
+        softTotal += card.getValue();
       }
     }
-    
-    if (isBust(hardTotal)) { // If the hardTotal is a bust
-      this.total = softTotal; // use the softTotal
-    } else { // If it is not a bust 
-      this.total = hardTotal; // use the hardTotal
+
+    if (isBust(hardTotal)) { // hardTotal always higher- if its a bust, the lower must be used.
+      this.total = softTotal;
+    } else {
+      this.total = hardTotal;
     }
-    
-    this.bust = isBust(this.total); // Which ever total is used check whether it is a bust or not.  
+
+    this.bust = isBust(this.total); // Which ever total is used check whether it is a bust or not.
   }
-  
+
   /**
-   * The isBust method checks to see if the 
-   * total of a hand is greater than 21. 
-   * @param number
-   * @return boolean value.  
+   * The isBust method checks to see if the total of a hand is greater than 21.
+   * 
+   * @param number number to check
+   * @return whether the number is a bust or not
    */
   private boolean isBust(int number) {
-    if (number > 21) { // if the variable is greater than 21
-      return true; // return true
+    if (number > 21) {
+      return true;
     } else {
-      return false; // If the variable is less than 21 return false.  
+      return false;
     }
   }
 
   /**
-   * The getCards method returns the Array of Card called cards. 
+   * The getCards method returns the Array of Card called cards.
+   * 
    * @return the cards
    */
   public Card[] getCards() {
-    return cards; // Return the Array called cards. 
+    return cards;
   }
 
   /**
-   * The setCards method sets the variable cards to 
-   * the Array called cards.
+   * The setCards method sets the variable cards to the Array called cards.
+   * 
    * @param cards the cards to set
    */
   public void setCards(Card[] cards) {
-    this.cards = cards; // Sets the variable cards to the Array of Card called cards
+    this.cards = cards;
   }
 
   /**
-   * The getTotal method returns the integer field called total.  
+   * The getTotal method returns the integer field called total.
+   * 
    * @return the total
    */
   public int getTotal() {
-    return total; // Return the total field. 
+    return total;
   }
 
   /**
-   * The setTotal method sets the variable total to 
-   * the field called total.
+   * The setTotal method sets the variable total to the field called total.
+   * 
    * @param total the total to set
    */
   public void setTotal(int total) {
-    this.total = total; // Sets the variable total to the int field called total. 
+    this.total = total;
   }
 
   /**
-   * The isBust method returns a boolean value
-   * of whether the hand is bust or not
+   * The isBust method returns a boolean value of whether the hand is bust or not
+   * 
    * @return the bust
    */
   public boolean isBust() {
-    return bust; // return bust
+    return bust;
   }
 
   /**
-   * The setBust method sets the bust field
-   * to true or false.  
+   * The setBust method sets the bust field to true or false.
+   * 
    * @param bust the bust to set
    */
   public void setBust(boolean bust) {
-    this.bust = bust; // Set the variable bust to the bust field .
+    this.bust = bust;
   }
 
 }
