1c5e0b09bac54f2e9fc2d202d1aca109692d0d7f
Will <grahamw0@students.rowan.edu>
2016-09-21 22:00:51 -0400

    Pruned comments.

--- Blackjack/src/blackjack/Player.java
+++ Blackjack/src/blackjack/Player.java
@@ -4,140 +4,135 @@
 package blackjack;
 
 /**
- * This is class "Player".  It will handle
- * all logic that pertains to the individual player in 
- * each hand.  
+ * This is class "Player". It will handle all logic that pertains to the individual player in each
+ * hand.
+ * 
  * @author Ryan Godfrey, Will Graham
- * @version 1.0		9/18/2016
+ * @version 1.0 9/18/2016
  */
 
 public class Player {
-  protected int money; // The int field called money.
-  protected int wins; // The int field called wins.
-  protected int pushes; // The int field called pushes. 
-  protected int losses; // The int field called losses. 
-  private int currentBet;  // Dealer won't ever have his own bet
-  protected Hand hand; // The field of type Hand called hand.  
+  protected int money;
+  protected int wins;
+  protected int pushes;
+  protected int losses;
+  private int currentBet; // Private because the inherited Dealer doesn't have their own bet.
+  protected Hand hand;
 
   /**
-   * The constructor for class "Player".
-   * It will initialize all fields upon creation.  
+   * The constructor for class "Player". It will initialize all fields upon creation.
    */
   public Player() {
-    this.money = 0; // Money is set to 0.
-    this.wins = 0; // Wins are set to 0.
-    this.pushes = 0; // Pushes are set to 0.
-    this.losses = 0; // Losses are set to 0.
-    this.currentBet = 0; // The current bet is set to 0.
-    this.hand = new Hand(); // Creates a new hand. 
+    this.money = 0;
+    this.wins = 0;
+    this.pushes = 0;
+    this.losses = 0;
+    this.currentBet = 0;
+    this.hand = new Hand();
   }
-  
+
   /**
-   * The win method will handle the 
-   * proper amount of winnings plus increment
-   * the win total. 
+   * The win method will handle the proper amount of winnings plus increment the win total.
    */
   public void win() {
-    money += currentBet * 2; // Update the money field by the current bet multiplied by 2
-    wins++; // increments the wins total. 
+    money += currentBet * 2;
+    wins++;
   }
 
   /**
-   * The push method will increment the 
-   * push total.  
+   * The push method will increment the push total.
    */
   public void push() {
-    pushes++; // Increment the pushes total. 
-  }
-  
-  /**
-   * The lose method will handle the 
-   * proper amount of lost and increment the losses
-   * total.  
-   */
-  public void lose() {
-    money -= currentBet; // Update the money field by subtracting the current bet. 
-    losses++; // Increment the losses field.  
+    pushes++;
   }
 
   /**
-   * The getMoney method will return the money field.  
+   * The lose method will handle the proper amount of lost and increment the losses total.
+   */
+  public void lose() {
+    money -= currentBet;
+    losses++;
+  }
+
+  /**
+   * The getMoney method will return the money field.
+   * 
    * @return the money
    */
   public int getMoney() {
-    return money; // Return money. 
+    return money;
   }
 
   /**
-   * The setMoney method will update the money
-   * field with the new variable money.  
+   * The setMoney method will update the money field with the new variable money.
+   * 
    * @param money the money to set
    */
   public void setMoney(int money) {
-    this.money = money; // Set money. 
+    this.money = money;
   }
 
   /**
-   * The getWins method will return
-   * the wins field. 
+   * The getWins method will return the wins field.
+   * 
    * @return the wins
    */
   public int getWins() {
-    return wins; // Return wins. 
+    return wins;
   }
 
   /**
-   * The getPushes method will return 
-   * the pushes field. 
+   * The getPushes method will return the pushes field.
+   * 
    * @return the pushes
    */
   public int getPushes() {
-    return pushes; // Return pushes. 
+    return pushes;
   }
 
   /**
-   * The getLosses method will return
-   * the losses field. 
+   * The getLosses method will return the losses field.
+   * 
    * @return the losses
    */
   public int getLosses() {
-    return losses; // Return losses. 
+    return losses;
   }
 
   /**
-   * The getCurrentBet method will return
-   * the currentBet field.  
+   * The getCurrentBet method will return the currentBet field.
+   * 
    * @return the currentBet
    */
   public int getCurrentBet() {
-    return currentBet; // Return currentBet.
+    return currentBet;
   }
 
   /**
-   * The setCurrentBet method will set the
-   * currentBet to itself. 
+   * The setCurrentBet method will set the currentBet to itself.
+   * 
    * @param currentBet the currentBet to set
    */
   public void setCurrentBet(int currentBet) {
-    this.currentBet = currentBet; // Set currentBet to parameter.  
+    this.currentBet = currentBet;
   }
 
   /**
-   * The getHand method will return
-   * the hand field. 
+   * The getHand method will return the hand field.
+   * 
    * @return the hand
    */
   public Hand getHand() {
-    return hand; // Return hand. 
+    return hand;
   }
 
   /**
-   * The setHand method will set 
-   * the hand variable to the hand field.  
+   * The setHand method will set the hand variable to the hand field.
+   * 
    * @param hand the hand to set
    */
   public void setHand(Hand hand) {
-    this.hand = hand;// Set hand to itself.  
+    this.hand = hand;
   }
 
 }
